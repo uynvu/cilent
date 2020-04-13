24e90def6d4386597fea8a6bc09953925854fa81
unknown <uyenvu2407@email.com>
2020-04-13 14:42:56 +0700

    first commit

---
+++ cilent/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cilent
+{
+    class Program
+    {
+        private const int BUFFER_SIZE = 1024;
+        private const int PORT_NUMBER = 9999;
+
+        static ASCIIEncoding encoding = new ASCIIEncoding();
+
+        public static void Main()
+        {
+
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                // 1. connect
+                client.Connect("127.0.0.1", PORT_NUMBER);
+                Stream stream = client.GetStream();
+
+                Console.WriteLine("Connected to Server.");
+                while (true)
+                {
+                    Console.Write("Enter your number(0-9): ");
+
+                    string str = Console.ReadLine();
+                    var reader = new StreamReader(stream);
+                    var writer = new StreamWriter(stream);
+                    writer.AutoFlush = true;
+
+                    // 2. send
+                    writer.WriteLine(str);
+
+                    // 3. receive
+                    str = reader.ReadLine();
+                    Console.WriteLine(str);
+                    if (str.ToUpper() == "BYE")
+                        break;
+                }
+                // 4. close
+                stream.Close();
+                client.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            Console.Read();
+        }
+    }
+}
